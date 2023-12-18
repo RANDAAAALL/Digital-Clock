8539571e4056b7c46fe597167a18af0cbff19c04
LSTRNDG <jenniecruz0506@gmail.com>
2023-12-18 19:39:33 +0800

    First Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const content = document.querySelector(".contents");
+const dates = document.querySelector(".dates");
+const days = document.querySelector(".days")
+
+const dayOfWeeks = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday"
+];
+
+function displayClock(){
+  let currentTime = new Date();
+
+  let month = currentTime.getMonth() + 1;
+  let date = currentTime.getDate();
+  let day = currentTime.getDay();
+  let newDay;
+
+  if(day >= 1 && day <= dayOfWeeks.length){
+    newDay = dayOfWeeks[day - 1];
+  }
+  let year = currentTime.getFullYear();
+  let hours = currentTime.getHours().toString().padStart(2, "0");
+  let mins = currentTime.getMinutes().toString().padStart(2, "0");
+  let seconds = currentTime.getSeconds().toString().padStart(2, "0");
+  
+  hours = hours > 12 ? hours - 12 : hours;
+  hours = hours === 0 ? 12 : hours;
+  
+  content.textContent = `${hours}:${mins}:${seconds} ${hours >= 12 ? "AM" : "PM"}`;
+  days.textContent = `${newDay}`
+  dates.textContent = `${month} / ${date} / ${year}`;
+}
+
+
+setInterval(displayClock, 1000);
+displayClock();
